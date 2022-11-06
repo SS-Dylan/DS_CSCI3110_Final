c08b6b5dfe82e4860400a7dd7b8d45dac3855c2e
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2022-11-06 16:39:37 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DS_CSCI3110_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
